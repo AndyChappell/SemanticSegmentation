d57ffe24a0e5138271f4fad7ff6b5d219578e706
StevenGreen1 <sg1sg2sg3@hotmail.co.uk>
2019-09-02 15:37:59 +0100

    Refactoring code.

--- Train.py
+++ Train.py
@@ -1,7 +1,7 @@
 # Code by GunhoChoi
 import torchvision
 
-from FusionNet import * 
+from FusionNet import *
 from UNet import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,25 +19,28 @@ args = parser.parse_args()
 batch_size = args.batch_size
 imageSize = 512
 learningRate = 0.0002
-nEpochs = 50
+nEpochs = 2
 
-# input pipeline
+# Input folder structure root/class1/Img1.jpg must be adhered
+# Input training data
+inputDirectoryTrain = "./TestData/train"
+imageDataTrain = datasets.ImageFolder(root=inputDirectoryTrain, transform = transforms.Compose([transforms.Resize(size=imageSize), transforms.CenterCrop(size=(imageSize,imageSize*2)), transforms.ToTensor(),]))
+imageBatchTrain = data.DataLoader(imageDataTrain, batch_size=batch_size, shuffle=True, num_workers=2)
 
-inputDirectory = "./Pandora_Data_Version2"
-imageData = datasets.ImageFolder(root=inputDirectory, transform = transforms.Compose([transforms.Resize(size=imageSize), transforms.CenterCrop(size=(imageSize,imageSize*2)), transforms.ToTensor(),]))
-imageBatch = data.DataLoader(imageData, batch_size=batch_size, shuffle=True, num_workers=2)
+# Input validation data
+inputDirectoryValidation = "./TestData/val/"
+imageDataValidation = datasets.ImageFolder(root=inputDirectoryValidation, transform = transforms.Compose([transforms.Resize(size=imageSize), transforms.CenterCrop(size=(imageSize,imageSize*2)), transforms.ToTensor(),]))
+imageBatchValidation = data.DataLoader(imageDataValidation, batch_size=batch_size, shuffle=True, num_workers=2)
 
-# initiate Generator
+nFiltersInitial = 16 # Initially 64
 
+# Initiate Generator
 if args.network == "fusionnet":
-    generator = nn.DataParallel(FusionGenerator(3,3,16),device_ids=[i for i in range(args.num_gpu)]).cuda()
-#    generator = nn.DataParallel(FusionGenerator(3,3,64),device_ids=[i for i in range(args.num_gpu)]).cuda()
+    generator = nn.DataParallel(FusionGenerator(3,3,nFiltersInitial),device_ids=[i for i in range(args.num_gpu)]) #.cuda()
 elif args.network == "unet":
-    generator = nn.DataParallel(UnetGenerator(3,3,16),device_ids=[i for i in range(args.num_gpu)]).cuda()
-#    generator = nn.DataParallel(UnetGenerator(3,3,64),device_ids=[i for i in range(args.num_gpu)]).cuda()
+    generator = nn.DataParallel(UnetGenerator(3,3,nFiltersInitial),device_ids=[i for i in range(args.num_gpu)]) #.cuda()
 
-# load pretrained model
-
+# Load pretrained model if it exists
 try:
     generator = torch.load('./model/{}.pkl'.format(args.network))
     print("\n--------model restored--------\n")
@@ -45,13 +48,11 @@ except:
     print("\n--------model not restored--------\n")
     pass
 
-# loss function & optimizer
-
+# Define the loss function and optimizer
 loss_fn = nn.MSELoss()
 gen_optimizer = torch.optim.Adam(generator.parameters(),lr=learningRate)
 
 # Training
-lossRecordFile = open('./{}_MSELoss'.format(args.network), 'w')
 lossTrainAverageList = []
 lossValidationAverageList = []
 epochList = []
@@ -62,17 +63,15 @@ for epoch in range(nEpochs):
     lossTrainAverage = 0
     trainSampleCounter = 0
 
-    for index, (image,label) in enumerate(imageBatch):
-        # Skip Validation Set
-        if int(label.item()) != 0:
-            continue
-
+    # Data Loader allows us to process the training and validation samples in batches, rather than all at once,
+    # which is essential for large data sets
+    for index, (image,label) in enumerate(imageBatchTrain):
         trainSampleCounter += 1
-        satel_image, map_image = torch.chunk(image, chunks=2, dim=3) 
+        inputImage, truthImage = torch.chunk(image, chunks=2, dim=3)
         gen_optimizer.zero_grad()
 
-        x = Variable(satel_image).cuda(0)
-        y_truth = Variable(map_image).cuda(0)
+        x = Variable(inputImage) #.cuda(0)
+        y_truth = Variable(truthImage) #.cuda(0)
 
         # Forward pass: compute predicted y by passing x to the model. Module objects
         # override the __call__ operator so you can call them like functions. When
@@ -83,9 +82,7 @@ for epoch in range(nEpochs):
         # Compute and print loss. We pass Tensors containing the predicted and true
         # values of y, and the loss function returns a Tensor containing the loss.
         loss = loss_fn(y_pred, y_truth)
-
         lossTrainAverage += loss.item()
-        lossRecordFile.write(str(loss.item())+"\n")
 
         # Backward pass: compute gradient of the loss with respect to all the learnable
         # parameters of the model. Internally, the parameters of each Module are stored
@@ -115,24 +112,30 @@ for epoch in range(nEpochs):
     lossValidationAverage = 0
     validationSampleCounter = 0
 
-    for index, (image,label) in enumerate(imageBatch):
-        # Skip Train Set
-        if int(label.item()) != 1:
-            continue
-
+    for index, (image,label) in enumerate(imageBatchValidation):
         validationSampleCounter += 1
-        x = Variable(satel_image).cuda(0)
-        y_truth = Variable(map_image).cuda(0)
+        inputImage, truthImage = torch.chunk(image, chunks=2, dim=3)
+
+        x = Variable(inputImage) #.cuda(0)
+        y_truth = Variable(truthImage) #.cuda(0)
         y_pred = generator.forward(x)
+
         loss = loss_fn(y_pred, y_truth)
         lossValidationAverage += loss.item()
 
     lossValidationAverage /= validationSampleCounter
     lossValidationAverageList.append(lossValidationAverage)
 
-plt.plot(epochList, lossTrainAverageList)
-plt.plot(epochList, lossValidationAverageList)
+fig, ax = plt.subplots()
+ax.set_title('Model Loss')
+ax.set_xlabel('Epoch')
+ax.set_ylabel('Average MSE Loss')
+ax.plot(epochList, lossTrainAverageList, label='Training Sample', c='darkred')
+ax.plot(epochList, lossValidationAverageList, label='Validation Sample', c='forestgreen')
+ax.legend(loc='upper right')
 plt.savefig('AverageLossVsTrainingEpoch')
 
-#        if _ > 5:
-#            sys.exit()
+with open('LossData.txt', 'w') as dataFile:
+    for idx, item in enumerate(epochList):
+        dataFile.write('Epoch : ' + str(item) + ', Train Loss : ' + str(lossTrainAverageList[idx]) + ', Validation Loss : ' + str(lossValidationAverageList[idx]) + '\n')
+
